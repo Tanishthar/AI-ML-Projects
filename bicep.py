ee868f6f56285a1527026acdd40ee590be21acb4
Tanishthar <64791373+Tanishthar@users.noreply.github.com>
2025-01-30 12:19:29 +0530

    Update bicep.py

--- bicep.py
+++ bicep.py
@@ -3,7 +3,7 @@ import mediapipe as mp
 import numpy as np
 import time
 
-# initialize mediapipe shit
+# initialize mediapipe
 mp_pose = mp.solutions.pose
 pose = mp_pose.Pose()
 mp_drawing = mp.solutions.drawing_utils
